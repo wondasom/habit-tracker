8cecc24a75c9a289373acf91a64bd56ff04406b7
dasomwon <wondasom93@gmail.com>
2020-09-30 17:43:02 +0200

    Updates

---
+++ precache-manifest.d0fe7874c6786fd8dffe1a53ba45264a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3dcaeed383ff3c2aa6e9a1f1ec951fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "772f0e8e41e7e6c597a3",
+    "url": "/static/css/main.f12fee4d.chunk.css"
+  },
+  {
+    "revision": "30ea2229acc1adba22ad",
+    "url": "/static/js/2.e8da4bea.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/static/js/2.e8da4bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "772f0e8e41e7e6c597a3",
+    "url": "/static/js/main.3eff3834.chunk.js"
+  },
+  {
+    "revision": "bae492c776d6e417a041",
+    "url": "/static/js/runtime-main.1b461360.js"
+  }
+]);
